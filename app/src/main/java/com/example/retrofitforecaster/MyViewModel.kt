81a48d30c2c35dd99d0a888d57f4deb5f6e5c99d
Lord World <mr.pro100y.patsan@mail.ru>
2022-03-17 14:12:08 +0700

    Final

--- app/src/main/java/com/example/retrofitforecaster/MyViewModel.kt
+++ app/src/main/java/com/example/retrofitforecaster/MyViewModel.kt
@@ -4,29 +4,23 @@ import androidx.lifecycle.ViewModel
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import timber.log.Timber
 
-class MyViewModel : ViewModel() {
-    private val weatherData: MutableList<List<ListItem>> by lazy {
-        mutableListOf<List<ListItem>>().also {
-            loadWeatherData()
-        }
+class MainViewModel : ViewModel() {
+    private var adapter = Adapter().apply {
+        setAdapter()
     }
 
-    fun getWeather(): List<ListItem> {
-        return weatherData[weatherData.lastIndex]
+    fun getAdapter(): Adapter {
+        return adapter
     }
 
-    private fun loadWeatherData() {
+    private fun setAdapter() {
         val mService = Common.retrofitService
-        Timber.plant(Timber.DebugTree())
-        Timber.d("Привет")
         mService.getWeatherList().enqueue(object : Callback<DataWeather> {
             override fun onResponse(call: Call<DataWeather>, response: Response<DataWeather>) {
                 val dataWeather = response.body() as DataWeather
-                weatherData.add(dataWeather.list)
+                adapter.submitList(dataWeather.list)
             }
-
             override fun onFailure(call: Call<DataWeather>, t: Throwable) {
             }
         })
